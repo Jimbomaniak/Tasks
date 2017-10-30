b7e45587d77c14085bf23d2267d1e695a16841d7
Jimbomaniak <jimboman54321@gmail.com>
2017-10-30 14:15:18 +0200

    remove ampty files

--- task4/app.js
+++ task4/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 
 
 const app = express();
-let db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
